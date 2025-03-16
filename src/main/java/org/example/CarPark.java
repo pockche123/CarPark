6ce337971b3462cc6efb362adb0a12f403149c57
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-03-16 11:46:06 +0000

    create CarPark class

--- src/main/java/org/example/CarPark.java
+++ src/main/java/org/example/CarPark.java
@@ -1,17 +1,57 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CarPark {
 
-    @Id
-    private int carParkId;
+//    private int carParkId;
     private int capacity;
     private int freeSpaces;
 
 
+    public void incrementSpaces(){
+        this.freeSpaces += 1;
+        System.out.println("The current free space is: " + freeSpaces);
+    }
+
+    public void decrementSpaces(){
+        this.freeSpaces -= 1;
+        System.out.println("The current free space is: " + freeSpaces);
+    }
+
+    public int spacesLeft(){
+        int spacesLeft = this.capacity - this.freeSpaces;
+        return spacesLeft;
+    }
 
 
-
-
-
+    public void update() {
+        //Place your carpark update logic here.
+        //The pseudo code illustrates some of the actions required
+        // in conjunction with other classes and will need further refinement.
+        //1.  poll car park components (sensor/etc).
+        //************************************************
+        //3.  if car present at entrance then
+        //4.    if ID valid && car park not full then
+        //5.      raise barrier and let car pass
+        //6.	  endif
+        //7.  endif
+        //************************************************
+        //9.  if car present at exit then
+        //10.   raise barrier and let car pass
+        //11.   update records and increment spaces
+        //12. endif
+        //************************************************
+        //14. if carpark full then
+        //15.   turn on full sign
+        //16: else
+        //17.   turn off full sign
+        //18. endif
+        //************************************************
+        //...and so on
+    }
     
 }
