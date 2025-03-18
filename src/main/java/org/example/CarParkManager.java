34c697f3ca4449e0e8a48f074c66b372700e9b7a
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-03-18 15:58:11 +0000

    final changes

--- src/main/java/org/example/CarParkManager.java
+++ src/main/java/org/example/CarParkManager.java
@@ -1,18 +1,14 @@
 package org.example;
 
-import org.example.parkingspots.ElectricVehicleSpotType;
-import org.example.parkingspots.HandiCappedSpotType;
-import org.example.parkingspots.NormalSpotType;
-import org.example.parkingspots.ParkingSpotType;
-import org.example.parkingstrategy.FirstAvailableParkingSpotStrategy;
-import org.example.parkingstrategy.NearestParkingSpotStrategy;
-import org.example.parkingstrategy.ParkingSpotStrategy;
+import org.example.builder.CarParkDirector;
 
 import java.util.Scanner;
 
 public class CarParkManager {
+
     private static Scanner stdin = new Scanner(System.in);
-    private CarPark carPark;
+
+    private CarParkDirector director;
 
 
 //    private Barrier barrier;
@@ -28,12 +24,39 @@ public class CarParkManager {
     public void enterCarPark()
     {
 //        car enter the car park
+        Car car = new Car("UJZ8884");
 //        sensor detects the car
+        Sensor sensor = new Sensor(car);
+
 //        car selects the parkingspot - if no parkingspot exits else
+        System.out.println("Please select a parking spot option (1-3)");
+        System.out.println("1. Standard");
+        System.out.println("2. Electric Vehicle");
+        System.out.println("3. Handicap");
+        int option = Integer.parseInt(stdin.nextLine());
+
+
+
+
 //        entry barrier opens
 //        car is registered into the registry
 //        sensor doesn't detect a car
 //        entry barrier closes
+
+    }
+
+    public boolean isNumber(String option){
+
+    }
+
+    public boolean checkValidOption(String option){
+        boolean numeric = false;
+        try{
+            int num = Integer.parseInt(option);
+        } catch(NumberFormatException e){
+
+        }
+
 
     }
 
