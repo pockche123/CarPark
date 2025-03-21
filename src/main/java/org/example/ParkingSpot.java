500de00e2303b718881aef518b275a4c55d42644
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-03-21 19:53:06 +0000

    refactor ParkingSpot

--- src/main/java/org/example/ParkingSpot.java
+++ src/main/java/org/example/ParkingSpot.java
@@ -6,17 +6,46 @@ import java.util.PriorityQueue;
 
 public class ParkingSpot {
     private int spotId;
-    private ParkingSpotType spotType;
+    private SpotType type;
+    private double distanceFromEntrance;
+
     private boolean taken = false;
-    public ParkingSpot(ParkingSpotType spotType){
-        this.spotType = spotType;
+    public ParkingSpot(int id, SpotType type, double distance){
+        this.spotId = id;
+        this.type = type;
+        this.distanceFromEntrance = distance
     }
 
     public void setTaken(boolean taken) {
         this.taken = taken;
     }
 
-    public ParkingSpotType getSpotType(){
-        return spotType;
+    public double getDistanceFromEntrance() {
+        return distanceFromEntrance;
     }
+
+    public void setDistanceFromEntrance(double distanceFromEntrance) {
+        this.distanceFromEntrance = distanceFromEntrance;
+    }
+
+    public boolean isTaken() {
+        return taken;
+    }
+
+    public SpotType getType() {
+        return type;
+    }
+
+    public void setType(SpotType type) {
+        this.type = type;
+    }
+
+    public int getSpotId() {
+        return spotId;
+    }
+
+    public void setSpotId(int spotId) {
+        this.spotId = spotId;
+    }
+
 }
